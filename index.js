7f083d6aebed783572eb21152b374ccfb6e6bed6
DanielG1298 <glaser.daniel1204@gmail.com>
2025-09-03 22:43:49 -0500

    finished workshop

--- index.js
+++ index.js
@@ -1,22 +1,23 @@
-let temp 
-function convertToCelsius(temp){return temp - 32 }
+
+function convertToCelsius(temp){return (temp - 32) * 5/9 }
 function describeTemperature(temp){
     const celsius = convertToCelsius(temp);
     let temperature;
-    if (temp < 0){
+    if (celsius < 0){
         temperature = "very cold"
     } 
-    else if (temp < 20){
+    else if (celsius < 20){
         temperature = "cold"
     }
-    else if (temp < 30){
+    else if (celsius< 30){
         temperature = "warm"
     }
-    else if (temp < 40){temperature = "hot"}
-    else if (temp >= 40){temperature = "very hot"
+    else if (celsius < 40){temperature = "hot"}
+    else if (celsius >= 40){temperature = "very hot"
     }
     return `it is ${temp} degrees outside or ${celsius} so it is ${temperature}`
 }
-const weather = prompt("weather:");
+let temp = parseFloat(prompt("weather:"))
+const weather = prompt;
 const description = describeTemperature(temp);
 alert(description);
